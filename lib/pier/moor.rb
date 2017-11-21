e9c8d920a9c369589b38a79fda80518c5f9b51f8
Matt Light <matt.light@lightdatasys.com>
2017-11-20 23:39:56 -0800

    Move `help` command to its own method

--- lib/pier/moor.rb
+++ lib/pier/moor.rb
@@ -39,22 +39,7 @@ module Pier
         exit 0
       end
 
-      unless ENV['PIER_MOOR_BASH'].to_s.empty?
-        cd_command = "\n  cd                Change directories to the root of a project"
-      end
-
-      puts <<~HELP
-        Usage:
-          moor COMMAND
-
-        Available commands:#{cd_command}
-          config            Set config option that all projects in workspace will have access to
-          docker            Run a docker command
-          docker-compose    Run a docker-compose command on the project found in the current working directory
-          install           Install a project
-          help              Output this help documentation
-HELP
-      exit 1
+      help
     end
 
     private
@@ -243,5 +228,24 @@ BANNER
     mapped_dir = pwd.sub(host_workspace, @workspace_config.workspace_root)
     throw :outside_workspace if mapped_dir == pwd && !File.exist?(mapped_dir)
     puts mapped_dir
+  end
+
+  def help
+    unless ENV['PIER_MOOR_BASH'].to_s.empty?
+      cd_command = "\n  cd                Change directories to the root of a project"
+    end
+
+    puts <<~HELP
+      Usage:
+        moor COMMAND
+
+      Available commands:#{cd_command}
+        config            Set config option that all projects in workspace will have access to
+        docker            Run a docker command
+        docker-compose    Run a docker-compose command on the project found in the current working directory
+        install           Install a project
+        help              Output this help documentation
+HELP
+    exit 1
   end
 end
